3800803fcf40b6ce2cc1bb9da8e1c4cd3a04d92d
desalvtj <desalvtj@mail.uc.edu>
2024-09-17 15:02:16 -0400

    Started

--- src/Hello.java
+++ src/Hello.java
@@ -1,8 +1,11 @@
 public class Hello {
     public static void main(String[] args) {
-        String name = "Tommy";
+        int age = 35;
+        double salary = 100000.55;
 
+        String name = "Tommy";
+        System.out.println("you said your age is " + age + " years old.");
+        System.out.println("you said your salary is " + salary + " per year.");
 System.out.println("Hello " + name);
     }
-
 }
